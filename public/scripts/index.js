dc407a3d8b67fe5fda0c1bef3cacd0905d3c0350
cheesy d <cheesyd64@hotmail.com>
2025-09-26 14:21:58 -0700

    refactored home page for beer info display / integrating edit

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -13,13 +13,60 @@ function displayBeers(beers){
     ul.innerHTML = "";
 
     beers.forEach(beer => {
-        const name = beer.name;
-        const image = "./img/" + `${beer.image}`;
+        //create elements
+        const name = document.createElement("span");
+        name.innerText = beer.name;
+        name.setAttribute("id", beer.id);
 
+        const image = document.createElement("img");
+        image.src = beer.image;
+        image.alt = beer.name;
+
+        const rating = document.createElement("span");
+        rating.innerText = `Rating: ${beer.rating}/5`;
+
+        const description = document.createElement("p");
+        description.innerText = beer.description;
+
+        const brewery = document.createElement("span");
+        brewery.innerText = `Brewery: ${beer.brewery}`;
+
+        const type = document.createElement("span");
+        type.innerText = `Type: ${beer.type}`;
+
+        const location = document.createElement("span");
+        location.innerText = `Location: ${beer.location}`;
+
+        const date = document.createElement("span");
+        date.innerText = `Added on: ${beer.date}`;
+
+        const form = document.createElement("form");
+        form.setAttribute("id", "editBeerForm");
+        form.setAttribute("action", "/editBeer");
+        form.setAttribute("method", "post");
+        form.setAttribute("enctype", "multipart/form-data");
+
+        const edit = document.createElement("button");
+        edit.innerText = "Edit";
+        edit.setAttribute("type", "submit");
+        edit.setAttribute("form", "editBeerForm");
+        edit.setAttribute("value", beer.id);
+
+        //append elements
         const li = document.createElement("li");
+        li.appendChild(image);
+        li.appendChild(name);
+        li.appendChild(rating);
+        li.appendChild(description);
+        li.appendChild(brewery);
+        li.appendChild(type);
+        li.appendChild(location);
+        li.appendChild(date);
+        
+        form.appendChild(edit);
 
         li.setAttribute("class", "beer-item");
-        li.innerHTML = `<img src="${image}" alt="${name}"><span>${name}</span>`;
+
         ul.appendChild(li);
     })
 }

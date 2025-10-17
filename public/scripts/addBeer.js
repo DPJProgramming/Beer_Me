52ed0918e8211a15d2edc15e0f86a61bf521455a
cheesy d <cheesyd64@hotmail.com>
2025-10-17 12:55:29 -0700

    Completed add beer validation on front end

--- public/scripts/addBeer.js
+++ public/scripts/addBeer.js
@@ -1,6 +1,65 @@
 window.onload = function() {    
     const form = document.getElementById("newBeerForm");
-    form.addEventListener("submit", getNewBeerInfo); 
+    form.addEventListener("submit", validate); 
+}
+
+function validate(event){
+    event.preventDefault();
+    const isValid = areFieldsValid();
+
+    if(!isValid){
+        return;
+    }
+    else{
+        getNewBeerInfo(event);
+    }
+}
+
+function areFieldsValid(){
+    let valid = true;
+    const name = document.getElementById("name").value;
+    const type = document.getElementById("type").value;
+    const rating = document.getElementById("rating").value;
+    const upload = document.getElementById("upload");
+    const image = upload.files[0];
+    const imageValid = document.getElementById("imageValid");
+
+    const nameValid = document.getElementById("nameValid");
+    const typeValid = document.getElementById("typeValid");
+    const ratingValid = document.getElementById("ratingValid");
+
+    if(!name){
+        nameValid.textContent = "Name is required";
+        valid = false;
+    }
+    else{
+        nameValid.textContent = "";
+    }
+    if(!type){
+        typeValid.textContent = "Type is required";
+        valid = false;
+    }
+    else{
+        typeValid.textContent = "";
+    }
+    if(!rating || isNaN(rating) || rating < 1 || rating > 5){
+        ratingValid.textContent = "Rating is required and must be a number between 1 and 5";
+        valid = false;
+    }
+    else{
+        ratingValid.textContent = "";
+    }
+    if(image){
+        const types = ['image/jpeg', 'image/png', 'image/gif', 'image/heic', 'image/heif'];
+        if(!types.includes(image.type)){
+            imageValid.innerText = "Invalid image type. Allowed types: JPEG, PNG, GIF, HEIC, HEIF.";
+            valid = false;
+        }
+        else{
+            imageValid.innerText = "";
+        }
+    }
+    return valid;
 }
 
 function getNewBeerInfo(event){

fcac456b4b90c2e5413f717b07916b821bb571d7
cheesy d <cheesyd64@hotmail.com>
2025-11-15 21:31:52 -0800

    Added Beer component

--- Beer_Me/app/components/Beer.tsx
+++ Beer_Me/app/components/Beer.tsx
@@ -2,13 +2,34 @@ import React from "react";
 import { View, Text, Image, Button, StyleSheet } from "react-native";
 
 export default function Beer() {
-    return (
+    const beer = {
+            name: "Sample Beer",
+            type: "IPA",
+            subType: "Hazy / New England IPA",
+            rating: 4.5,
+            imageUrl: "./assets/images/Delete.png",
+            brewery: "Sample Brewery",
+            description: "A delicious sample beer.",
+            location: "Sample Location"
+    };
+    return (        
         <View>
-            <Text>Beer Component</Text>
+            <Text>{beer.name}</Text>
+            <Text>{beer.type}</Text>
+            <Text>{beer.subType}</Text>
+            <Text>{beer.rating}</Text>
+            <Image style={BeerStyles.image} source={require("../../assets/images/Delete.png")} />
+            <Text>{beer.brewery}</Text>
+            <Text>{beer.description}</Text>
+            <Text>{beer.location}</Text>
         </View>
     );
 }
 
 const BeerStyles = StyleSheet.create({
-    
+    image: {
+        width: 100,
+        height: 100,
+        marginBottom: 12
+    }
 });
